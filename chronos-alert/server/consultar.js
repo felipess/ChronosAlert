7bc87ae27d8198cde9946624dd4614c57b515477
Felipe Soares <felipediogenes87@gmail.com>
2024-11-13 18:49:54 -0300

    bibliotecas puppeteer date-fns dotenv e teste

--- chronos-alert/server/consultar.js
+++ chronos-alert/server/consultar.js
@@ -4,8 +4,6 @@ import { format, startOfDay, addDays } from 'date-fns';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
-const interval = 10;
-
 // Obter o diretório atual do módulo
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,6 +12,31 @@ const __dirname = dirname(__filename);
 dotenv.config({ path: resolve(__dirname, '../../.env') });
 
 const JFUrl = process.env.JF_URL;
+//teste de importação do valor de variavéis do .env
+console.log("JFUrl:", JFUrl);
 
+//teste date-fns
 const dataInicio = format(startOfDay(new Date()), 'yyyy-MM-dd');
 const dataFim = format(addDays(startOfDay(new Date()), 1), 'yyyy-MM-dd');
+console.log("dataInicio", dataInicio);
+console.log("dataFim", dataFim);
+
+(async () => {
+    try {
+        // Inicia o navegador
+        const browser = await puppeteer.launch({ headless: true }); // O 'headless: true' significa que o navegador será invisível.
+        const page = await browser.newPage();
+
+        // Navega para uma página
+        await page.goto('https://www.google.com');
+
+        // Extrai o título da página para verificar se o Puppeteer conseguiu acessar corretamente
+        const pageTitle = await page.title();
+        console.log('Título da página:', pageTitle);
+
+        // Fecha o navegador
+        await browser.close();
+    } catch (error) {
+        console.error('Erro ao rodar o Puppeteer:', error);
+    }
+})();
