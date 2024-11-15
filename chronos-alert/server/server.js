a79c8c50caf8acad77e0928f7ac4eb64ad5efa3a
Felipe Soares <felipediogenes87@gmail.com>
2024-11-15 17:06:59 -0300

    rotas de postits adicionadas

--- chronos-alert/server/server.js
+++ chronos-alert/server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,6 +9,7 @@ const port = process.env.PORT;
 const mongoUrl = process.env.MONGO_URL;
 const result_collection = process.env.COLLECTION_RESULT;
 const notif_collection = process.env.COLLECTION_NOTIF;
+const postit_collection = process.env.COLLECTION_POSTIT;
 const dbName = process.env.DATABASE;
 
 if (!mongoUrl) {
@@ -65,7 +66,112 @@ app.get('/api/notificacoes', async (req, res) => {
     }
 });
 
+// Endpoint para criar um novo post-it
+app.post('/api/postits', async (req, res) => {
+    try {
+        const { db } = await connectToMongo();
+        const collection = db.collection(postit_collection);
+
+        await collection.createIndex({ createdAt: 1 }, { expireAfterSeconds: 172800 }); // Exclui documentos após 2 dias
+        const newPostIt = req.body;
+
+        // Adiciona o campo 'createdAt' e as informações do 'usuario'
+        const postItWithDate = {
+            ...newPostIt,
+            createdAt: new Date(),
+            usuario: newPostIt.usuario,
+        };
+
+        // Insira o novo post-it na coleção
+        const result = await collection.insertOne(postItWithDate);
+
+        // Se a inserção ok, retorna o novo post-it e ID
+        if (result.acknowledged) {
+            const insertedPostIt = { _id: result.insertedId, ...postItWithDate };
+            res.status(201).json(insertedPostIt);
+        } else {
+            res.status(500).send('Erro ao criar post-it');
+        }
+    } catch (error) {
+        console.error('Erro ao criar post-it:', error);
+        res.status(500).send('Erro ao criar post-it');
+    }
+});
+
+// Endpoint para obter todos os post-its
+app.get('/api/postits', async (req, res) => {
+    try {
+        const { db } = await connectToMongo();
+        const collection = db.collection(postit_collection);
+        const postIts = await collection.find({}).toArray();
+        res.json(postIts);
+    } catch (error) {
+        console.error('Erro ao obter post-its:', error);
+        res.status(500).send('Erro ao obter post-its');
+    }
+});
+
+/* Endpoint para atualizar um post-it */
+app.put('/api/postits/:id', async (req, res) => {
+    try {
+        const { db } = await connectToMongo();
+        const collection = db.collection(postit_collection);
+
+        const updatedPostIt = { text: req.body.text };
+
+        // Se o usuário for passado, incluir o campo usuario na atualização
+        if (req.body.usuario) {
+            updatedPostIt.usuario = req.body.usuario;
+        }
+
+        // Atualize o 'createdAt' para a data atual
+        updatedPostIt.createdAt = new Date();
+
+        // Operação de atualização
+        const updateFields = {
+            $set: updatedPostIt
+        };
+
+        // Primeiro, tenta encontrar o post-it para garantir que 'createdAt' seja configurado apenas quando necessário
+        const postIt = await collection.findOne({ _id: new ObjectId(req.params.id) });
+
+        // Realiza a atualização do post-it
+        const result = await collection.findOneAndUpdate(
+            { _id: new ObjectId(req.params.id) },
+            updateFields,
+            { returnDocument: 'after' }
+        );
+
+        if (!result.text) {
+            return res.status(404).send('Post-it não encontrado');
+        }
+
+        res.json(result);
+
+    } catch (error) {
+        console.error('Erro ao atualizar post-it:', error);
+        res.status(500).send('Erro ao atualizar post-it');
+    }
+});
+
+// Endpoint para excluir um post-it
+app.delete('/api/postits/:id', async (req, res) => {
+    try {
+        const { db } = await connectToMongo();
+        const collection = db.collection(postit_collection);
+        const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+
+        if (result.deletedCount === 1) {
+            res.status(204).send();
+        } else {
+            res.status(404).send('Post-it não encontrado');
+        }
+    } catch (error) {
+        console.error('Erro ao excluir post-it:', error);
+        res.status(500).send('Erro ao excluir post-it');
+    }
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando...`);
 });
-
