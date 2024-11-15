e1b29aeb8b390ee90429a9bdc88865031c433762
Felipe Soares <felipediogenes87@gmail.com>
2024-11-15 16:17:33 -0300

    Api resultados

--- chronos-alert/server/server.js
+++ chronos-alert/server/server.js
@@ -1,13 +1,54 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+const mongoUrl = process.env.MONGO_URL;
+const result_collection = process.env.COLLECTION_RESULT;
+const dbName = process.env.DATABASE;
+
+if (!mongoUrl) {
+    console.error('A variável de ambiente MONGO_URL não está definida no server.');
+    process.exit(1);
+}
 
 app.use(cors()); // Habilitar CORS
 app.use(express.json()); // Habilita análise de corpo JSON em requisições
+
+let db, client;
+
+// Função para conectar ao MongoDB e armazenar conexão
+async function connectToMongo() {
+    if (!db || !client) {
+        client = new MongoClient(mongoUrl);
+        try {
+            await client.connect();
+            console.log("Conectado ao MongoDB");
+            db = client.db(dbName);
+        } catch (error) {
+            console.error('Erro ao conectar ao MongoDB:', error);
+            throw new Error('Erro ao conectar ao MongoDB');
+        }
+    }
+    return { db, client };
+}
+
+// Endpoint para obter os resultados
+app.get('/api/resultados', async (req, res) => {
+    try {
+        const { db } = await connectToMongo();
+        const collectionResultados = db.collection(result_collection);
+        const ultimoResultado = await collectionResultados.findOne({}, { sort: { _id: -1 } }); // Ordena por _id decrescente
+        res.json(ultimoResultado);
+
+    } catch (error) {
+        console.error('Erro ao obter resultados:', error);
+        res.status(500).send('Erro ao obter resultados');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando...`);
