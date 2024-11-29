29651ba3d06f5c70edfe09d8c9a64588db5cb49f
Felipe Soares <felipediogenes87@gmail.com>
2024-11-29 11:20:10 -0300

    fix const footer

--- chronos-alert/client/src/components/Footer.js
+++ chronos-alert/client/src/components/Footer.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 const Footer = ({ tema }) => {
-    const headerClasses = tema === 'dark'
+    const footerClasses = tema === 'dark'
         ? 'custom-headerFooter-dark'
-        : 'custom-headerFooter-white'
+        : 'custom-headerFooter-white';
 
     return (
-        <footer className={`py-4 px-4 ${headerClasses} text-white`}>
+        <footer className={`py-4 px-4 ${footerClasses} text-white`}>
             <div className="container text-center">
                 <p>&copy; {new Date().getFullYear()} Felipe Diógenes.</p>
-                <div className="mt-2">
-                    <a href="https://facebook.com" className="mx-2 text-white"><i className="fab fa-facebook"></i></a>
-                    <a href="https://twitter.com" className="mx-2 text-white"><i className="fab fa-twitter"></i></a>
-                    <a href="https://instagram.com" className="mx-2 text-white"><i className="fab fa-instagram"></i></a>
+                <div className="mt-2 d-flex justify-content-center">
+                    <a href="https://facebook.com" className="mx-2 text-white fs-5">
+                        <i className="fab fa-facebook"></i>
+                    </a>
+                    <a href="https://twitter.com" className="mx-2 text-white fs-5">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href="https://instagram.com" className="mx-2 text-white fs-5">
+                        <i className="fab fa-instagram"></i>
+                    </a>
                 </div>
             </div>
         </footer>
